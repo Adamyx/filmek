401c8ebcbde5303307565db3b1b72414acbf8ff8
Adamyx <cseh.adam@kreativszolnok.hu>
2026-01-29 12:20:06 +0100

    Js_card

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const cim = document.getElementById("cim");
 const mufaj = document.getElementById("mufaj");
-const kep = document.getElementById("kep");
+const kep = document.getElementById("kep-url");
 const body = document.querySelector("body");
-const btn = document.getElementById("btn");
-
-const tablediv = document.createElement("div");
+const btn = document.getElementById("hozzaadas");
 
 btn.addEventListener("click", function() {
-    const tr = document.createElement("tr");
-    const td1 = document.createElement("td");
-    const td2 = document.createElement("td");
-    const td3 = document.createElement("td");
+    const carddiv = document.createElement("div");
+    const cardimg = document.createElement("img");
+    const carddivbody = document.createElement("div");
+    const cardtitle = document.createElement("h5");
+    const cardtext = document.createElement("p");
+    const carddelete = document.createElement("input");
 
-    td1.innerHTML = cim.value;
-    td2.innerHTML = mufaj.value;
-    td3.innerHTML = kep.value;
+    if (cim.value == "" || kep.value == "") {
+        alert("Valameik mező üres, kérem töltse ki az összes mezőt!");
+        return
+    }
+
+    carddiv.classList.add("card");
+    cardimg.classList.add("card-img-top");
+    carddivbody.classList.add("card-body");
+    cardtitle.classList.add("card-title");
+    cardtext.classList.add("card-text");
+    
+    carddelete.type = "button";
+    carddelete.classList.add("btn", "btn-danger", "text-white");
 })
